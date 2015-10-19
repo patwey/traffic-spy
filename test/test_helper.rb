cd7fac9feb38d302ba9bb06e169d49e603ddd36c
Tyler Komoroske <tjkomor@clemson.edu>
2015-10-19 17:34:55 -0600

    added to test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,23 @@ Bundler.require
 
 require File.expand_path("../../config/environment", __FILE__)
 require 'minitest/autorun'
+require 'minitest/pride'
 require 'capybara'
 
+DatabaseCleaner.strategy = :truncation, {except: %w[public.schema_migrations]}
+
+class Minitest::Test
+  def setup
+    DatabaseCleaner.start
+  end
+
+  def teardown
+    DatabaseCleaner.clean
+  end
+end
+
 Capybara.app = TrafficSpy::Server
+
+class FeatureTest < Minitest::Test
+  include Capybara::DSL
+end
