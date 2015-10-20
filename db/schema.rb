db2663ebbcb00ffcef4550fa8c245c1cf5a0a0b5
Pat Wey <patrickwey@gmail.com>
2015-10-20 14:28:52 -0600

    renamed apps table to sources

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151020174001) do
+ActiveRecord::Schema.define(version: 20151020200949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "apps", force: :cascade do |t|
-    t.text     "identifier"
-    t.text     "root_url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "sources", force: :cascade do |t|
+    t.text "identifier"
+    t.text "root_url"
   end
 
 end
