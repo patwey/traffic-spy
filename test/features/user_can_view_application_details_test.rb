35a04156f94356c865b8fed8adf62153eb19baac
Tyler Komoroske <tjkomor@clemson.edu>
2015-10-21 19:14:38 -0600

    added more feature tests

--- test/features/user_can_view_application_details_test.rb
+++ test/features/user_can_view_application_details_test.rb
@@ -22,7 +22,8 @@ class ViewApplicationDetailsTest < FeatureTest
     TrafficSpy::PayloadCreator.process(params)
   end
 
-  def test_user_can_view_most_requested_urls
+  def test_it_returns_error_page_if_identifier_does_not_exist
+    skip
     create_source
     create_payload({url: "http://jumpstartlab.com/tyler"})
     create_payload({url: 'http://jumpstartlab.com/blog'})
@@ -31,7 +32,17 @@ class ViewApplicationDetailsTest < FeatureTest
     visit '/sources/jumpstartlab'
     assert_equal '/sources/jumpstartlab', current_path
 
-    refute page.has_content?("Identifier doesn't exist")
+    assert page.has_content?("Identifier doesn't exist")
+  end
+
+  def test_user_can_view_most_requested_urls
+    create_source
+    create_payload({url: "http://jumpstartlab.com/tyler"})
+    create_payload({url: 'http://jumpstartlab.com/blog'})
+    create_payload({url: 'http://jumpstartlab.com/blog', requested_at: Time.now})
+
+    visit '/sources/jumpstartlab'
+    assert_equal '/sources/jumpstartlab', current_path
 
     within '#application-details' do
       assert has_content?("Most Requested URL's")
@@ -52,8 +63,6 @@ class ViewApplicationDetailsTest < FeatureTest
     visit '/sources/jumpstartlab'
     assert_equal '/sources/jumpstartlab', current_path
 
-    refute page.has_content?("Identifier doesn't exist")
-
     within '#application-details' do
       assert has_content?("Web Browser Breakdown")
 
@@ -73,14 +82,50 @@ class ViewApplicationDetailsTest < FeatureTest
     visit '/sources/jumpstartlab'
     assert_equal '/sources/jumpstartlab', current_path
 
-    refute page.has_content?("Identifier doesn't exist")
-
     within '#application-details' do
       assert has_content?("Operating System Breakdown")
 
       within '#os-breakdown' do
         assert has_content?('Apple iOS 2')
         assert has_content?('Windows 95 1')
+      end
+    end
+  end
+
+  def test_user_can_see_screen_resolutions
+    create_source
+    create_payload({url: "http://jumpstartlab.com/tyler"})
+    create_payload({url: 'http://jumpstartlab.com/blog'})
+    create_payload({url: 'http://jumpstartlab.com/blog', requested_at: Time.now})
+
+    visit '/sources/jumpstartlab'
+    assert_equal '/sources/jumpstartlab', current_path
+
+    within '#application-details' do
+      assert has_content?("Screen Resolution")
+
+      within '#screen-resolution' do
+        assert has_content?('1820 x 1960 2')
+        assert has_content?('750 x 1080 1')
+      end
+    end
+  end
+
+  def test_user_can_average_response_times
+    create_source
+    create_payload({url: "http://jumpstartlab.com/tyler"})
+    create_payload({url: 'http://jumpstartlab.com/blog'})
+    create_payload({url: 'http://jumpstartlab.com/blog', requested_at: Time.now})
+
+    visit '/sources/jumpstartlab'
+    assert_equal '/sources/jumpstartlab', current_path
+
+    within '#application-details' do
+      assert has_content?("URL by Response Time")
+
+      within '#response-time' do
+        assert has_content?('http://jumpstartlab.com/blog 20s')
+        assert has_content?('http://jumpstartlab.com 1s')
       end
     end
   end
