4a1c0b4d306c2b4d79108755b3a59efc1208939b
Pat Wey <patrickwey@gmail.com>
2015-10-23 18:23:26 -0600

    linked tables via foreign keys

--- app/models/event_name.rb
+++ app/models/event_name.rb
@@ -1,4 +1,4 @@
 class TrafficSpy::EventName < ActiveRecord::Base
   validates :event_name, uniqueness: true
-  has_many :payloads
+  has_many :payloads, through: :event_name_id
 end
--- app/models/payload.rb
+++ app/models/payload.rb
@@ -1,7 +1,7 @@
 class TrafficSpy::Payload < ActiveRecord::Base
-  belongs_to :source
+  belongs_to :source, foreign_key: :source_id
+  belongs_to :url, foreign_key: :url_id
+  belongs_to :request_type, foreign_key: :request_type_id
+  belongs_to :event_name, foreign_key: :event_name_id
   validates :sha, uniqueness: true
-
-  # { scope: :sha,
-  # message: "Request payload has already been received." }
 end
