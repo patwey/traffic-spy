96bcf2a284b234ad3623674f48128fc2cef4f3d1
Pat Wey <patrickwey@gmail.com>
2015-10-25 13:04:48 -0600

    added source_id to Urls

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151022171052) do
+ActiveRecord::Schema.define(version: 20151025175802) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,8 @@ ActiveRecord::Schema.define(version: 20151022171052) do
   end
 
   create_table "urls", force: :cascade do |t|
-    t.text "url"
+    t.text    "url"
+    t.integer "source_id"
   end
 
 end
