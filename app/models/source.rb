16c32913e8b1f7be7f0941f654fc62dfc21ce5d3
Pat Wey <patrickwey@gmail.com>
2015-10-25 13:08:42 -0600

    source model 'has many' urls

--- app/models/source.rb
+++ app/models/source.rb
@@ -1,7 +1,7 @@
 class TrafficSpy::Source < ActiveRecord::Base
   validates_presence_of :identifier, :root_url
   has_many :payloads
-  has_many :urls, through: :payloads
+  has_many :urls
   has_many :request_types, through: :payloads
   has_many :event_names, through: :payloads
   has_many :user_agents
